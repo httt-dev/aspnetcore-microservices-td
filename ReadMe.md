c8b8ebcb8a66a0293789215f49274e615e233b44
HoaNguyen <httt.dev@gmail.com>
2022-12-11 21:16:48 +0700

    chinh sua version cua elasticsearch /add serilog to productAPI

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,35 @@
 ## AspnetCore Microservices Tutorial
 
-**Development Environment**
-Cd to folder contains dockerxxx.yml file and run this cmd:
-	docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d --remove-orphans
+## Chuan bi moi truong 
+* Install dotnet core version in the file `global.json`
+* Visual studio CE 2022+
+
+## How to run project
+
+Run command for build project 
+
+```powershell
+dotnet build
+```
+Goto folder contain file `docker-compose`
+
+1. Using docker-compose
+
+```Powershell
+
+docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d --remove-orphans
+
+```
+## Application URLs - LOCAL enviroment ( Docker container)
+
+## Docker Application URLs -LOCAL enviroment ( Docker Container)
+
+- Portainer : http://localhost:9000 - user : admin / pass : Abc12345
+- Kibana : http://localhost:5601 - user : admin / pass : Abc12345
+- RabbitMQ : http://localhost:15672 - user : admin / pass : Abc12345
+
+2. Using Visual Studio 2022
+- Open aspnetcore-microservices-td\aspnetcore-microservices.sln
+- Run compound to start multi projects
+
+## Application URLs - DEVELOPMENT Env 
--- src/Services/Product.API/Program.cs
+++ src/Services/Product.API/Program.cs
@@ -1,23 +1,51 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+using Serilog;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-var app = builder.Build();
+Log.Information("Starting Product API up");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+            .WriteTo.Console(
+                outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+            .Enrich.FromLogContext()
+            .ReadFrom.Configuration(ctx.Configuration
+        ));
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}catch(Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shutdown Product API complete");
+    Log.CloseAndFlush(); 
 }
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
